acaceab5f8d76f65ee1a52c10e4496aae278dbcd
cotton <chinoknct@gmail.com>
2020-06-16 14:58:30 +0900

    [add]環境構築
    環境構築およびメニューバーの追加を行いました。

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-app>
+      <v-app-bar app>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+        </v-app-bar-nav-icon>
+      </v-app-bar>
+      <nuxt />
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
